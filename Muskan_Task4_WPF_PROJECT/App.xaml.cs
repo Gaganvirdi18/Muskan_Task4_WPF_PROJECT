c6f6ed6551ce2db4178461ba93f1344bfeafff57
Gaganvirdi18 <156145073+Gaganvirdi18@users.noreply.github.com>
2025-02-11 16:21:43 -0500

    Add project files.

---
+++ Muskan_Task4_WPF_PROJECT/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Muskan_Task4_WPF_PROJECT
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
